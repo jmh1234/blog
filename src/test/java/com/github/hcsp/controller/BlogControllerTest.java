c1ba27ef334685e09a83f0888e6502544db02fc0
JJB <923268677@qq.com>
2020-09-25 15:41:50 +0800

    修改BlogControllerTest使之通过CI测试

--- src/test/java/com/github/hcsp/controller/BlogControllerTest.java
+++ src/test/java/com/github/hcsp/controller/BlogControllerTest.java
@@ -2,14 +2,11 @@ package com.github.hcsp.controller;
 
 import com.alibaba.fastjson.JSONObject;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import com.github.hcsp.dao.BlogDao;
 import com.github.hcsp.entity.Blog;
 import com.github.hcsp.entity.BlogResult;
 import com.github.hcsp.entity.User;
 import com.github.hcsp.service.BlogService;
 import com.github.hcsp.service.impl.AuthService;
-import com.github.hcsp.service.impl.BlogServiceImpl;
-import com.github.hcsp.service.impl.UserService;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -18,17 +15,15 @@ import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.http.MediaType;
-import org.springframework.mock.web.MockHttpSession;
-import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.MvcResult;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 
-import javax.servlet.http.HttpSession;
 import java.nio.charset.Charset;
+import java.util.Optional;
 
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyInt;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
@@ -39,63 +34,38 @@ class BlogControllerTest {
     @Mock
     private BlogService blogService;
     @Mock
-    private UserService userService;
-    @Mock
-    private AuthenticationManager authenticationManager;
+    private AuthService authService;
 
-    private MockMvc userMockMvc;
-    private MockMvc blogMockMvc;
-    private BCryptPasswordEncoder bCryptPasswordEncoder = new BCryptPasswordEncoder();
+    private MockMvc mockMvc;
 
     @BeforeEach
-    public void setUp() {
-        AuthService authService = new AuthService(userService);
-        blogMockMvc = MockMvcBuilders.standaloneSetup(new BlogController(authService, blogService)).build();
-        userMockMvc = MockMvcBuilders.standaloneSetup(new UserController(userService, authService, authenticationManager)).build();
+    void setUp() {
+        mockMvc = MockMvcBuilders.standaloneSetup(new BlogController(authService, blogService)).build();
     }
 
     @Test
-    void create() throws Exception {
-        // login info
-        JSONObject userJson = new JSONObject();
-        userJson.put("username", "jhgfcky3");
-        userJson.put("password", "123456");
-
-        // blog info
+    void createWithoutLogin() throws Exception {
         JSONObject blogJson = new JSONObject();
         blogJson.put("title", "1");
         blogJson.put("content", "2");
         blogJson.put("description", "3");
-
-        // login judge
-        blogMockMvc.perform(post("/blog")
+        mockMvc.perform(post("/blog")
                 .contentType(MediaType.APPLICATION_JSON)
                 .content(new ObjectMapper().writeValueAsBytes(blogJson)))
                 .andExpect(status().isOk())
                 .andExpect(mvcResult -> Assertions.assertTrue(mvcResult.getResponse().getContentAsString(Charset.forName("UTF-8")).contains("登录后才能操作")));
+    }
 
-        Mockito.when(userService.getUserInfoByUsername("jhgfcky3"))
-                .thenReturn(new User(1, "jhgfcky3", bCryptPasswordEncoder.encode("123456")));
+    @Test
+    void createBlog() throws Exception {
+        JSONObject blogJson = new JSONObject();
+        blogJson.put("title", "1");
+        blogJson.put("content", "2");
+        blogJson.put("description", "3");
+        Mockito.when(authService.getCurrentUser()).thenReturn(Optional.of(new User(1, "mockUser", "")));
 
-        // start login
-        MvcResult mvcResult = userMockMvc.perform(post("/auth/login")
-                .header("user-agent", "[{\"key\":\"User-Agent\",\"value\":\"Mozilla/5.0 (iPhone; CPU iPhone OS 9_1 like Mac OS X) AppleWebKit/601.1.46 (KHTML, like Gecko) Version/9.0 Mobile/13B143 Safari/601.1\",\"description\":\"\",\"type\":\"text\",\"enabled\":true}]", "Mozilla/5.0 (iPhone; CPU iPhone OS 9_1 like Mac OS X) AppleWebKit/601.1.46 (KHTML, like Gecko) Version/9.0 Mobile/13B143 Safari/601.1")
-                .contentType(MediaType.APPLICATION_JSON)
-                .content(new ObjectMapper().writeValueAsBytes(userJson)))
-                .andExpect(status().isOk())
-                .andExpect(result -> Assertions.assertTrue(result.getResponse().getContentAsString(Charset.forName("UTF-8")).contains("登录成功")))
-                .andReturn();
-
-        HttpSession httpSession = mvcResult.getRequest().getSession();
-        if (httpSession == null) {
-            throw new RuntimeException();
-        }
-
-        Mockito.when(blogService.addBlogInfo(new Blog())).thenReturn(BlogResult.success("博客新建成功",
-                new Blog(1, "1", "2", "3", new User(1, "jhgfcky3", bCryptPasswordEncoder.encode("123456")))));
-
-        blogMockMvc.perform(post("/blog")
-                .session((MockHttpSession) httpSession)
+        Mockito.when(blogService.addBlogInfo(any())).thenReturn(BlogResult.success("博客新建成功", null));
+        mockMvc.perform(post("/blog")
                 .contentType(MediaType.APPLICATION_JSON)
                 .content(new ObjectMapper().writeValueAsBytes(blogJson)))
                 .andExpect(status().isOk())
@@ -106,7 +76,13 @@ class BlogControllerTest {
     }
 
     @Test
-    void getBlogDetail() {
+    void getBlogDetail() throws Exception {
+        Mockito.when(blogService.getBlogInfoById(anyInt())).thenReturn(new Blog(
+                1, "", "", "", new User(1, "mockUser", "")
+        ));
+        mockMvc.perform(get("/blog/1"))
+                .andExpect(status().isOk())
+                .andExpect(mvcResult -> Assertions.assertTrue(mvcResult.getResponse().getContentAsString(Charset.forName("UTF-8")).contains("获取成功")));
     }
 
     @Test
